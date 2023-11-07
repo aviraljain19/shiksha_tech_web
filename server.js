24e32df214911f704ccba9862ad341bc1cca31b2
Aviral Jain <aviraljainltr@gmail.com>
2023-11-08 02:32:54 +0530

    Created express server & route for different pages

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.set("view engine","ejs");
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000,()=>{
+    console.log("Server is running");
+})
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+
+app.get('/search',(req,res)=>{
+    res.render("search");
+})
+
+app.get('/lectureScreen',(req,res)=>{
+    res.render("lectureScreen");
+})
+
+app.get('/intern',(req,res)=>{
+    res.render("intern");
+})
