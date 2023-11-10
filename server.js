269f8801e81e0481defc91288f830710b18d0884
Aviral Jain <aviraljainltr@gmail.com>
2023-11-10 13:59:03 +0530

    Implemented registration & login authentication.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 mongoose
   .connect(
     "mongodb+srv://aviraljain:jK499gdydHWxE10J@shikshatech.evqd53u.mongodb.net/?retryWrites=true&w=majority"
@@ -17,6 +21,14 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+
+const studentSchema = mongoose.Schema({
+  username: String,
+  fullname: String,
+  password: String,
+});
+
+const Student = mongoose.model("Student", studentSchema);
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -34,8 +46,34 @@ app.get("/intern", (req, res) => {
   res.render("intern");
 });
 
+app.post("/login", async (req, res) => {
+    const studentFound = await Student.findOne({username:req.body.username})
+    if(!studentFound){
+        return res.json({
+            msg:"invalid credentials"
+        })
+    }
+    if(req.body.password === studentFound.password){
+        res.render('index')
+    }else{
+        return res.json({
+            msg:"invalid credentials"
+        })
+    }
+});
+
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.post("/register", async (req, res) => {
+ const { fullname, username, password } = req.body;
+  const student = await Student.create({
+    fullname,
+    username,
+    password,
+  });
+  res.redirect("/login");
 });
 
 app.get("/register", (req, res) => {
